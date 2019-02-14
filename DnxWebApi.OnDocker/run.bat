326140dce8764d0201dc1f54c0e8b8073a8f3792
Rickard Robin <rickard.robin@gmail.com>
2019-02-14 14:13:25 +0100

    getting close

--- DnxWebApi.OnDocker/run.bat
+++ DnxWebApi.OnDocker/run.bat
@@ -2,5 +2,6 @@ echo off
 REM -d is for detaching (run container in bg)
 REM -p is for port mapping
 REM docker run -it --rm -p 8081:8081 --name dnxweb_app dnxweb
+REM docker run -it --rm --name dnxweb_app dnxweb
 echo on
-docker run -it --rm --name dnxweb_app dnxweb
+docker run -d --name dnxweb_debug dnxweb
--- NginX.Front/run.bat
+++ NginX.Front/run.bat
@@ -1,5 +1,8 @@
 echo off
 REM -d is for detaching (run container in bg)
 REM -p is for port mapping
+REM docker run --name nginxfront_debug -d nginxfront
+REM docker run --name nginxfront_debug -it nginxfront
+REM docker run -it nginxfront
 echo on
-docker run -t --name nginxfront_app -d nginxfront
+docker run -d -p 8080:8080 nginxfront
