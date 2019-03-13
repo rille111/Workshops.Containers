46e46a1abfeb738d17921effb2f91d36c6ceee75
riro01 <rickard.robin@afaforsakring.se>
2019-03-13 09:11:47 +0100

    remove start.bat with misleading name

--- kubernetes/start.bat
+++
@@ -1,8 +0,0 @@
-echo off
-REM Need to use the Hyper-V switch since we're on windows.
-REM Also need to put ourselves in C: (same folder as minikube is installed) because it cant find the iso of minikube otherwise
-echo on
-
-c:
-cd\
-minikube start --vm-driver hyperv --hyperv-virtual-switch "minikube_network_switch"
